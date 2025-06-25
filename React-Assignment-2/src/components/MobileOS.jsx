e9f1308fea1438b3992987aa4a71f9c85afeb868
13-Bhupendra <bhupendrakp2008@gmail.com>
2025-06-25 13:26:05 +0530

    Assignment-2

---
+++ React-Assignment-2/src/components/MobileOS.jsx
@@ -0,0 +1,25 @@
+function MobileOS(){
+    return (
+       <div className="container">
+         <h1>Mobile Operating System</h1>
+        <ul>
+            <li>Android</li>
+            <li>Blackberry</li>
+            <li>iPhone</li>
+            <li>Windows Phones</li>
+        </ul>
+
+        <hr />
+
+        <h1>Mobile Manufacturers</h1>
+        <ul>
+            <li>Samsung</li>
+            <li>HTC</li>
+            <li>Micromax</li>
+            <li>Apple</li>
+        </ul>
+       </div>
+    )
+}
+
+export default MobileOS
