d1e88efb559e6681f23f489c79f837d62336e137
nehapatil0411 <8561nehapatil@gmail.com>
2025-07-02 18:32:49 +0530

    Assignment-4 setup

--- React-Assignment-4 (Counter)/src/Components/Counter.jsx
+++ React-Assignment-4 (Counter)/src/Components/Counter.jsx
@@ -2,18 +2,25 @@ import { useState } from "react";
 
 function Counter() {
   const [Counter, setCounter] = useState(0);
+
+  const handleCounter = (type)=> {
+      type == "INCREMENT" ? 
+      (Counter < 20 ? setCounter(Counter + 1) : setCounter(Counter) ) : 
+      (Counter > 0 ? setCounter(Counter - 1) : setCounter(Counter))
+  }
+
   return (
     <div>
       <h1>Counter</h1>
 
-      <button onClick={() =>   Counter < 20 ? setCounter(Counter + 1) : setCounter(Counter)} >
-        +
+      <button onClick={() => handleCounter("DECREMENT")} >
+        -
       </button>
 
       <span>{Counter}</span>
 
-      <button onClick={() => Counter > 0 ? setCounter(Counter - 1) : setCounter(Counter)}>
-        -
+      <button onClick={() => handleCounter("INCREMENT")}>
+        +
       </button>
 
       <hr />
